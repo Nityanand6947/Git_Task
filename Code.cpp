d1fb4daf0befbb1ce39643f86a51249745988c18
Nityanand6947 <nityanandtamsoy17@gmail.com>
2024-11-25 15:25:38 +0530

    new branch

--- Code.cpp
+++ Code.cpp
@@ -3,7 +3,11 @@ using namespace std;
 
 int main(){
     string name;
+    int age;
     cout<<"Enter your name"<<endl;
     getline(cin,name);
+    cout<<"Enter you age"<<endl;
+    cin>>age;
     cout<<"Hey it's "<<name<<endl;
+    cout<<"And age is "<<age<<endl;
 }
